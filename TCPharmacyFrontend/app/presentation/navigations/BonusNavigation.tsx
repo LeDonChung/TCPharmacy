297457d35ea535aadb4a05465001e9cb41754ffb
LeDonChung <ledonchung12a2@gmail.com>
2024-10-11 21:23:53 +0700

    Updated InAppNavigation

---
+++ TCPharmacyFrontend/app/presentation/navigations/BonusNavigation.tsx
@@ -0,0 +1,14 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { MyQrScreen } from "../pages/inapp/account/MyQrScreen";
+import React from "react";
+import { BonusPointScreen } from "../pages/inapp/BonusPointScreen";
+const Stack = createStackNavigator();
+export const BonusNavigation = () => {
+    return (
+        <>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="bonusPoint" component={BonusPointScreen} />
+            </Stack.Navigator>
+        </>
+    )
+}
