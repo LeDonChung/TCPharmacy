e9e61d6fd73ecb06a10e457416294e0f829f80c3
thahhtuyenn <thanhtuyen9623@gmail.com>
2024-10-29 20:24:33 +0700

    create header tab 'Tu Van'

--- TCPharmacyFrontend/app/presentation/pages/inapp/ConsultScreen.tsx
+++ TCPharmacyFrontend/app/presentation/pages/inapp/ConsultScreen.tsx
@@ -1,11 +1,87 @@
-import { SafeAreaView, Text } from "react-native"
-import React from "react"
+import { Text, TouchableOpacity, View } from "react-native"
+import React, { useEffect, useState } from "react"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { useNavigation } from "@react-navigation/native"
+import { Colors } from "../../styles/Colors"
+import { GlobalStyles } from "../../styles/GlobalStyles"
+import IconF5 from "react-native-vector-icons/FontAwesome5"
+import IconF from "react-native-vector-icons/Feather"
+
+const messageList = [{
+    id: 1,
+    message: "Xin chào",
+    isMe: true
+},
+{
+    id: 2,
+    message: "Chào bạn",
+    isMe: false
+},
+{
+    id: 3,
+    message: "Bạn cần tư vấn gì không?",
+    isMe: false
+},
+{
+    id: 4,
+    message: "Bên mình có bán kẹo ngậm không đường không ạ, nếu có cho em xin giá ạ",
+    isMe: true
+},
+{
+    id: 5,
+    message: "Vui lòng đợi một vài phút để ad kiểm tra giúp bạn nhé.",
+    isMe: false
+}]
 
 export const ConsultScreen = () => {
+
+    const navigation = useNavigation();
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState(messageList);
+    useEffect(() => {
+        // setMessages(messageList);
+    }, []);
+
+    //send message
+    const sendMessage = () => {
+        if (message.trim() === "") {
+            return;
+        }
+        const newMessage = {
+            id: messages.length + 1,
+            message: message,
+            isMe: true
+        }
+        setMessages([...messages, newMessage]);
+        setMessage("");
+    }
+
+    // Lấy ngày, tháng, và năm hiện tại
+    const getCurrentDate = () => {
+        const date = new Date();
+
+        const day = date.getDate();
+        const month = date.getMonth() + 1; // getMonth() trả về tháng từ 0 đến 11, nên cần +1
+        const year = date.getFullYear();
+
+        // Định dạng lại thành chuỗi "dd/mm/yyyy"
+        return `${day}/${month}/${year}`;
+    };
+
     return (
         <>
             <SafeAreaView>
-                <Text> Consult Screen </Text>
+                <View style={{ height: 70, width: '100%', backgroundColor: Colors.primary, flexDirection: 'row', alignItems: 'center', justifyContent:"space-between" }}>
+                    <TouchableOpacity style={{ marginHorizontal: 15, justifyContent: "center", alignContent: "center" }} onPress={() => { navigation.goBack() }}>
+                        <IconF5 name="chevron-left" size={35} style={{ color: Colors.secondary }} />
+                    </TouchableOpacity>
+                    <Text style={[GlobalStyles.textStyle, { fontSize: 20, fontWeight: '700', color: Colors.secondary, textAlign: 'center' }]}>
+                        Dược sĩ TC Parmacy
+                    </Text>
+                    <TouchableOpacity style={{ marginHorizontal: 15, justifyContent: "center", alignContent: "center" }} onPress={() => {  }}>
+                        <IconF name="phone-call" size={35} style={{ color: Colors.secondary }} />
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         </>
     )
