d5a9cf9d40493bd46e20b19688ea7a81df69b0dd
LeDonChung <ledonchung12a2@gmail.com>
2024-10-31 21:50:13 +0700

    Added: CheckoutScreen

--- TCPharmacyFrontend/app/presentation/navigations/InAppNavigation.tsx
+++ TCPharmacyFrontend/app/presentation/navigations/InAppNavigation.tsx
@@ -6,6 +6,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { PageNavigation } from "./PageNavigation";
 import { BonusNavigation } from "./BonusNavigation";
 import { ChatScreen } from "../pages/inapp/ChatScreen";
+import { CartScreen } from "../pages/inapp/CartScreen";
+import { CheckoutScreen } from "../pages/inapp/CheckoutScreen";
 
 const Stack = createStackNavigator();
 
@@ -19,6 +21,7 @@ export const InAppNavigation = () => {
                 <Stack.Screen name="accountMavigation" component={AccountNavigation} />
                 <Stack.Screen name="bonusNavigation" component={BonusNavigation} />
                 <Stack.Screen name="chatScreen" component={ChatScreen} />
+                <Stack.Screen name="checkoutScreen" component={CheckoutScreen} />
             </Stack.Navigator>
             
         </>
