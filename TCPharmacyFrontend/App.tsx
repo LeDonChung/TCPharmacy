5c76acb5093e3f406c326e7e6efcfcd684b797c0
thahhtuyenn <thanhtuyen9623@gmail.com>
2024-11-04 14:28:21 +0700

    Test

--- TCPharmacyFrontend/App.tsx
+++ TCPharmacyFrontend/App.tsx
@@ -4,7 +4,7 @@ import { AppNavigation } from './app/presentation/navigations/AppNavigation';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/presentation/redux/store';
-
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
     <>
